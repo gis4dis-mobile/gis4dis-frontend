f95d9026c292cb791e38503d6e76c4e8039e465e
SLeitgeb <leitgeb.simon@gmail.com>
2017-10-03 12:13:44 +0200

    Application skeleton and basic structure created, two bottom sheet templates added.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 var mobile = screen.width < 800;
 
-var map = L.map('map').setView([49.2, 16.6], 13);
+var map = L.map('map', {
+	center: [49.2, 16.6],
+	zoom: 13,
+	zoomControl: false
+});
 
 L.tileLayer(mobile ? 'http://{s}.osm.rrze.fau.de/osmhd/{z}/{x}/{y}.png' : 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+$(document).ready(function(){
+	$('#bottom-sheet').modal();
+})
+
+function loadToBottomSheet(template) {
+	$('#bottom-sheet').load("templates/" + template);
+}
